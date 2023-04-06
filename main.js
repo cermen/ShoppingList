5d564de68a6e17519587240132aa08ae68a4b1f5
cermen <yjs01016@naver.com>
2023-04-06 23:07:32 +0900

    enter 키 입력 시 항목 추가 기능 구현

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 const add = document.getElementById('add');
 const section = document.querySelector('section');
-add.addEventListener("click", (event) => {
-    const item = document.getElementById('item').value;
-    section.innerHTML += `<p>${item}<span><i class="fa-solid fa-xmark"></i></span></p>`;
+const item = document.getElementById('item');
+let count = 0;
+
+const addItem = (event) => {
+    if (item.value !== "") {
+        section.innerHTML += `<p id="${count++}">${item.value}<span><i class="fa-solid fa-xmark"></i></span></p>`;
+        item.value = "";
+    }
+};
+
+add.addEventListener("click", addItem);
+item.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        addItem();
+    }
 });
 
 const items = document.querySelectorAll('section > p');
-const deleteButtons = items.querySelectorAll('span');
+const deleteButtons = document.querySelectorAll('section > p > span');
